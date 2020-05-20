2e0af1a0a8a6707fbdcd345843ccbedc3f301215
bob <bcoop713@gmail.com>
2020-05-19 22:11:14 -0400

    refactor

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,49 +1,8 @@
-import depict, { isValid, explain, conform } from "../lib";
-import { boolean, string } from "../lib";
+import depict, { isValid, explain, conform, execute } from "../lib";
+import { boolean, string, list } from "../lib";
 
-describe("isValid", () => {
-  test("positive", () => {
-    expect(isValid(boolean, false)).toBe(true);
-  });
-  test("negative", () => {
-    expect(isValid(boolean, "asdf")).toBe(false);
-  });
-  test("custom depiction", () => {
-    const boat = depict("boat", boolean);
-    expect(isValid(boat, false)).toBe(true);
-  });
-});
-
-describe("explain", () => {
-  test("simple", () => {
-    expect(explain(boolean, "asdf")).toEqual([
-      {
-        value: "asdf",
-        depiction: "boolean",
-        validator: "boolean",
-        dPath: ["boolean"],
-        vPath: []
-      }
-    ]);
-  });
-  test("custom depiction", () => {
-    const boat = depict("boat", boolean);
-    expect(explain(boat, "asdf")).toEqual([
-      {
-        value: "asdf",
-        depiction: "boat",
-        validator: "boolean",
-        dPath: ["boat"],
-        vPath: []
-      }
-    ]);
-  });
-});
-
-describe("conform", () => {
-  test("simple", () => {
-    expect(conform(boolean, "asdf")).toBe(true);
-  });
+test("simple execute", () => {
+  expect(execute(list(list(string)), [["asdf"], ["qwert"]])).toEqual([]);
 });
 
 /*
